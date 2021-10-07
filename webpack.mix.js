ed7d965d11b16ff9108a4d6ae753783bab0df5d0
93Kamuran <ke@inensus.com>
2021-10-07 13:01:35 +0300

    app.js renamed as main.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,7 @@
 const mix = require('laravel-mix')
 const tailwindcss = require('tailwindcss')
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/main.js', 'public/js/app.js')
     .vue()
     .sass('resources/sass/app.scss', 'public/css')
     .options({
