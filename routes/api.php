aae65984aa35ed989603374e3eeb459bd2728f17
93Kamuran <ke@inensus.com>
2021-10-07 13:15:29 +0300

    API routes generated.

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\PdfResourceController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'pdf-resources'], static function () {
+    Route::post('/', [PdfResourceController::class, 'store']);
+    Route::post('/{pdfResource}', [PdfResourceController::class, 'update']);
+    Route::get('/', [PdfResourceController::class, 'index']);
+    Route::delete('/{pdfResource}', [PdfResourceController::class, 'destroy']);
+    Route::get('/{pdfResource}/download', [PdfResourceController::class, 'download']);
 });
